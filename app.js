c6272cbc810435e576438bf0ad1295982c11a9ea
Sari Bäckman <sari.m.backman@gmail.com>
2023-07-18 15:34:28 +0300

    tehtävä 11.6

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 3000");
-});
+  console.log('server started on port 3000')
+})
