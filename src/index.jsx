c3cefcced5714776a010601d35f2ddf3a9a53f24
Sari Bäckman <sari.m.backman@gmail.com>
2023-08-01 16:08:44 +0300

    tehtävä 11.18, fix build

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
-/* eslint-disable */
-import akuankka from 'akuankka'
-/* eslint-enable */
 
 ReactDOM.render(<App /> , document.getElementById('app'))
