efbe154a5a9a173012892bd9e6b0ae892b348768
Sari Bäckman <sari.m.backman@gmail.com>
2023-08-01 15:53:58 +0300

    tehtävä 11.18, lisätään buildin rikkoja

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
+import akuankka
 
 ReactDOM.render(<App /> , document.getElementById('app'))
